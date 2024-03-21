a816d6419cf563c800b770e1f7c19386f2c9212a
Priyadharshini-Nagarajan <161636455+Priyadharshini-Nagarajan@users.noreply.github.com>
2024-03-21 19:48:39 +0530

    Add files via upload

--- BasicTaskManagementSys/TreeSetJ.java
+++ BasicTaskManagementSys/TreeSetJ.java
@@ -1,1 +1,47 @@
+import java.util.*;
 
+class Task implements Comparable<Task> {
+    private String name;
+    private int priority;
+
+    public Task(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public int compareTo(Task other) {
+        return Integer.compare(this.priority, other.priority);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Priority: " + priority + ")";
+    }
+}
+
+public class TreeSetJ {
+    public static void main(String[] args) {
+        TreeSet<Task> taskSet = new TreeSet<>();
+
+        // Adding tasks
+        taskSet.add(new Task("Write report", 2));
+        taskSet.add(new Task("Prepare presentation", 1));
+        taskSet.add(new Task("Review document", 3));
+        taskSet.add(new Task("Attend meeting", 2));
+
+        // Displaying tasks in sorted order based on priority
+        System.out.println("Tasks:");
+        for (Task task : taskSet) {
+            System.out.println(task);
+        }
+    }
+}
